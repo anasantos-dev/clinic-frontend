2943e6ac81c38029b3f89a09790803de9c5d4608
anasantos-dev <acws_ss@hotmail.com>
2025-09-30 20:56:26 -0300

    feat: initialize backend with login screen

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent }, // funciona com standalone
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onSubmit() {
+    if (this.username && this.password) {
+      alert(`Login enviado! Usuário: ${this.username}`);
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
+  }
+}
